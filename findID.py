723f339f0b9a7ae73b748846e397658ef2d840ab
Sem Reiestad <afotball@gmail.com>
2021-11-03 11:42:03 +0100

    findID

--- findID.py
+++ findID.py
@@ -1,139 +1,66 @@
 import requests
 import bs4
-import pandas as pd
+import time
+import os
 
+dirName = input("Name of new directory: ")
+os.mkdir(dirName)
+file = open(dirName + "/playerID.txt", "w")
 
-# Fixa spelarnamn
-res = requests.get('https://www.futbin.com/22/players?page=1&league=4&position=CDM,CM,CAM&pdribbling=75,0')
+# Find players from 1st page
+res = requests.get(
+    'https://www.futbin.com/players?page=1&wf=4&skills=4&version=icons')
 soup = bs4.BeautifulSoup(res.text, features='lxml')
-
-# ta fram namn
 names = soup.select('td > div > div > a')
 
-# rating
-ratings = soup.select('.form')
+# Number of pages to iterate through
+try:
+    pages = soup.select('.page-link')
+    numberPages = int(pages[-2].get_text().strip())
+except:
+    numberPages = 1
 
-# ta fram antal sidor
-pages = soup.select('.page-link')
-
-# rakna fram hur maanga sidor som ska gaas igenom
-sistaSidan = (len(pages))
-antalSidor = pages[sistaSidan-2].get_text()
-
-# initiera lista med alla namn
-listName = []
+# Find all links to players
 listLink = []
-listRating = []
-
-# spara namn och fullstandig lank till lista
-for name in names:
-    name = name.get_text()
-    listName.append(name)
-
 for name in names:
     link = name.get('href')
     link = "https://www.futbin.com" + link
     listLink.append(link)
-    
-for rating in ratings:
-    rating = rating.get_text()
-    listRating.append(rating)
-############
 
-############
+if numberPages >= 2:
+    for i in range(2,numberPages+1):
+        link = 'https://www.futbin.com/players?page='+ str(i) + '&wf=4&skills=4&version=icons'
+        res = requests.get(link)
+        soup = bs4.BeautifulSoup(res.text, features='lxml')
+        names = soup.select('td > div > div > a')
+        for name in names:
+            link = name.get('href')
+            link = "https://www.futbin.com" + link
+            listLink.append(link)
 
-# ta fram namn for resterande sidor paa hemsidan
-for page in range(int(antalSidor)):
-    #print(f"{page+1}/{int(antalSidor)-1}")
-    # Fixa spelarnamn
-    res = requests.get('https://www.futbin.com/22/players?page='+page+'&league=4&position=CDM,CM,CAM&pdribbling=75,0')
-    soup = bs4.BeautifulSoup(res.text, features='lxml')
-
-    # ta fram namn
-    names = soup.select('td > div > div > a')
-    # = soup.findAll('span', {'class': 'form rating ut22 icon gold rare'})
-    ratings = soup.select('.form')
-    
-    # spara namn och fullstandig lank till lista
-    for name in names:
-        name = name.get_text()
-        listName.append(name)
-
-    for name in names:
-        link = name.get('href')
-        link = "https://www.futbin.com" + link
-        listLink.append(link)
-    
-    for rating in ratings:
-        rating = rating.get_text()
-        listRating.append(rating)
-
-############
-
-print(listRating)
-# init databas
-database = {}
-
-# Fixa saa att alla spelarstats sparas
+# Fixa så att alla spelarstats sparas
 playerCounter = 0
 for links in listLink:
-    # FIXA VARJE SPELARES STATS
     res = requests.get(links)
+    time.sleep(1)
     soup = bs4.BeautifulSoup(res.content, features='lxml')
-    
-    # spara ner stats och vad det ar for stats
-    stats = soup.select('.stat_val')
-    statText = soup.select('div.stat_holder_sub.left_stat_name > span')
-    
-    # ID
-    IDtext = soup.select('#page-data')
-    spelarID = IDtext[0].get('data-player-id').strip()
-    
-    # INFO
-    infoStats = soup.select('.table-row-text')
-    infoText = soup.select('tr > th')
-    
-    # Header name
-    hName = soup.select('.header_name')
-    headerName = hName[0].getText().strip()
-    
-    # forklarar vart i processen det ar
-    #print(str(playerCounter + 1) + "/" + str(len(listName)), end=": ")
-    print(headerName)
 
-    # lagg alla stats i en dict
-    tempStats = {}
-    
-    # lagg info i en temporar dict
-    statsCounter = 0
-    for i in infoText:
-        namnPaaInfo = i.getText().strip()
-        vardePaaInfo = infoStats[statsCounter].getText().strip()
-        
-        # visa endast cm paa langd
-        if namnPaaInfo == "Height":
-            vardePaaInfo = vardePaaInfo[:3]
-        
-        # lagg till i tempStats
-        tempStats.update({namnPaaInfo : vardePaaInfo})
-        statsCounter += 1
-    
-    # lagg stats, headerName och ID i samma temporara dict
-    counter = 0
-    for i in statText:
-        namnPaaStats = (i.getText().strip())
-        vardePaaStats = (stats[counter].getText().strip())
-        tempStats.update({namnPaaStats : vardePaaStats, "ID" : spelarID, "Header Name": headerName})
-        counter += 1
+    # Player ID
+    player_id = soup.find('div', {'id': 'page-info'}).get('data-player-resource')
+    cName = soup.select('.pcdisplay-name')
+    cardName = cName[0].getText().strip()
+    rt = soup.select('.pcdisplay-rat')
+    rating = rt[0].getText().strip()
 
-    # uppdatera databasen for spelaren
-    database.update({tempStats['ID'] : tempStats})
+    # förklarar vart i processen det är
+    print(str(playerCounter + 1) + "/" + str(len(listLink)), end=": ")
+    print(cardName)
+
+    string = "'" + cardName + " " + rating + "': " + player_id + ","
+    file.write(string + "\n")
+
     playerCounter += 1
-    
-############
-# skriv till excel
 
-df = pd.DataFrame(database).T
-df.to_excel('Futbin - FIFA 21.xlsx')
+file.close()
 
-print("Klar ")
+print("Check file")
